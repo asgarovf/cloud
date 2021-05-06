80d8b56fc57d6f0f82c9e51676ee189740f980bf
asgarovf <fasgarov2002@gmail.com>
2021-05-06 21:00:54 +0400

    fix: navbar bug

--- controllers/index_controller.js
+++
@@ -1,9 +0,0 @@
-var express = require("express");
-var router = express.Router();
-const i18next = require("i18next");
-class IndexController {
-  static list(req, res, next) {
-    res.render("index", { title: "Express" });
-  }
-}
-module.exports = IndexController;
